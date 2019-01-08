6a22be89dfbcf7e381db5f0518d937949c1fb0a0
Benoit Dequidt <benoit.dequidt@gmail.com>
2019-01-08 22:15:11 +0800

    Implement basic setup.sh
    It is still missing checks for
        - git & vim packages being installed
        - theme and font required for tmux
        - font for vim
    
    Also likely more checking and options

--- setup.sh
+++ setup.sh
@@ -1,3 +1,42 @@
 #!/bin/sh
 
-echo "To be implemented"
+log_status() {
+    echo " -- $1..."
+}
+
+log_section() {
+    echo "==============="
+
+}
+
+install_file() {
+    name=$(basename $1)
+    dest="$HOME/.$name"
+    src=$1
+    if [ -f $dest ]; then
+        diff $dest $src &> /dev/null
+        if [ $? -ne 0 ]; then
+            log_status "Moving $dest -> $dest.back"
+            mv $dest "$dest.back"
+        else
+            return
+        fi
+    fi
+    ln -s $src $dest
+}
+
+ROOT=$(dirname $0)
+
+log_section
+log_status "Installing Vim"
+install_file "$ROOT/vim/vimrc"
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone --quiet https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+fi
+log_section
+log_status "Installing Tmux"
+install_file "$ROOT/tmux/tmux.conf"
+install_file "$ROOT/tmux/tmux.theme"
+log_section
+log_status "Done!"
