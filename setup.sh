3eb9034e0e49dd85f936cd06183660b8476a7645
Benoit Dequidt <benoit.dequidt@gmail.com>
2019-07-24 21:15:52 +0800

    Change git alias s to status -sb

--- setup.sh
+++ setup.sh
@@ -42,7 +42,7 @@ install_file "$ROOT/git/gitignore"
 log_status "Installing git aliases"
 git config --global alias.c commit
 git config --global alias.l log
-git config --global alias.s status
+git config --global alias.s status -sb
 git config --global alias.ca commit --amend
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.f fetch
