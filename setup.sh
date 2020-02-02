794cd408797a2f5f7b298d75d655e658aed56246
Benoit Dequidt <benoit.dequidt@gmail.com>
2020-02-02 09:47:24 +0800

    git: add alias 'st' for normal status

--- setup.sh
+++ setup.sh
@@ -50,5 +50,6 @@ git config --global alias.f fetch
 git config --global alias.cob checkout -b
 git config --global alias.co checkout
 git config --global alias.d diff
+git config --global alias.st status
 log_section
 log_status "Done!"
